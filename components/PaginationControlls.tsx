b8c6c1576fc6e31fc5fc7ab616a0b6a6e7431672
abdullahi-abdiaziz <abdullahikawte2019@gmail.com>
2024-11-24 20:54:27 +0300

    improve the ui ofpagination controls

--- components/PaginationControlls.tsx
+++ components/PaginationControlls.tsx
@@ -1,8 +1,20 @@
 "use client";
 import React from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { BiSolidLeftArrow, BiSolidRightArrow } from "react-icons/bi";
+import {
+  BiRightArrow,
+  BiRightArrowAlt,
+  BiSolidLeftArrow,
+  BiSolidLeftArrowSquare,
+  BiSolidRightArrow,
+  BiSolidRightArrowAlt,
+} from "react-icons/bi";
 import Link from "next/link";
+import {
+  ArrowLeftSquare,
+  ArrowRightFromLine,
+  ArrowRightSquare,
+} from "lucide-react";
 
 type PaginationProps = {
   hasNextPage: boolean;
@@ -16,12 +28,12 @@ const PaginationControlls = ({ hasNextPage, hasPrevPage }: PaginationProps) => {
   const page = searchParams.get("page") ?? "1";
   const per_page = searchParams.get("per_page") ?? "9";
   return (
-    <div className="flex gap-2 items-center justify-center mt-10 bg-green-50 dark:bg-green-950 px-2 py-1 rounded-md w-fit mx-auto">
+    <div className="flex gap-2 items-center justify-center mt-10  px-2 py-1 rounded-md w-fit mx-auto">
       <Link
         className={`${
           !hasPrevPage
-            ? "text-gray-400 dark:text-gray-600"
-            : "text-black dark:text-white"
+            ? "text-blue-300 dark:text-blue-300"
+            : "text-blue-600 dark:text-blue-600"
         } flex justify-center items-center"`}
         href={`${
           hasPrevPage
@@ -30,19 +42,30 @@ const PaginationControlls = ({ hasNextPage, hasPrevPage }: PaginationProps) => {
         }`}
         {...(hasPrevPage ? {} : { disabled: true })}
       >
-        <BiSolidLeftArrow />
+        <div className="px-2 py-1 rounded-md  flex gap-1 items-center">
+          <ArrowLeftSquare size={16} />
+          <p>Previous</p>
+        </div>
       </Link>
 
-      <div className="text-black dark:text-white">
-        page <span className="font-bold">{page}</span> of{" "}
-        <span className="font-bold">{Math.ceil(10 / Number(per_page))}</span>
+      <div className="text-black dark:text-white flex gap-1">
+        <span className="font-bold  w-8 text-center  rounded-sm bg-white dark:bg-slate-900 shadow-lg border">
+          {hasPrevPage && Number(page) - 1}
+        </span>
+
+        <span className="font-bold px-3 py-0.5 rounded-sm shadow-lg bg-yellow-200 text-yellow-900 border scale-110 ml-2 mr-2">
+          {page}
+        </span>
+        <span className="font-bold w-8 text-center border py-0.5 rounded-sm bg-white dark:bg-slate-900 shadow-lg">
+          {hasNextPage && Number(page) + 1}
+        </span>
       </div>
 
       <Link
         className={`${
           !hasNextPage
-            ? "text-gray-400 dark:text-gray-600"
-            : "text-black dark:text-white"
+            ? "text-blue-300 dark:text-blue-300"
+            : "text-blue-600 dark:text-blue-600"
         }  flex items-center justify-center"`}
         href={`${
           hasNextPage
@@ -51,7 +74,10 @@ const PaginationControlls = ({ hasNextPage, hasPrevPage }: PaginationProps) => {
         }`}
         {...(hasNextPage ? {} : { disabled: true })}
       >
-        <BiSolidRightArrow />
+        <div className="px-2 py-1 rounded-md flex gap-1 items-center">
+          <p>Next</p>
+          <ArrowRightSquare size={16} />
+        </div>
       </Link>
     </div>
   );
